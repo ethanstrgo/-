9be57ecab72fe328d60d119bd07c1a8f719a7d58
Ethan_ Y- Y <34997045+ethanstrgo@users.noreply.github.com>
2018-01-11 23:54:12 +0800

    Create 链表成对调换.py

---
+++ 链表成对调换.py
@@ -0,0 +1,15 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+ 
+class Solution:
+    # @param a ListNode
+    # @return a ListNode
+    def swapPairs(self, head):
+        if head != None and head.next != None:
+            next = head.next
+            head.next = self.swapPairs(next.next)
+            next.next = head
+            return next
+        return head
